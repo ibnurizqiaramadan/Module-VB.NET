39bc12c8df53103f0a56dcedae458a6d043ab0ba
DevNesas15 <32134721+DevNesas15@users.noreply.github.com>
2017-10-07 10:12:50 +0700

    Add files via upload

---
+++ Belajar/Belajar/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tampilkan_data() 'ini akan error karena anda belum membuat sub baru
+    End Sub
+
+    Sub tampilkan_data()
+        Try
+            SqlCon.Open()
+
+            SQLCMD = New MySqlCommand("SELECT * FROM t_user", SqlCon)
+            SQLDA = New MySqlDataAdapter(SQLCMD)
+            SQLDS = New DataSet
+            SQLDA.Fill(SQLDS)
+            If SQLDS.Tables.Count > 0 Then
+                DataGridView1.DataSource = SQLDS.Tables(0)
+            End If
+
+            SqlCon.Close()
+        Catch ex As Exception
+            MsgBox("Gagal : " + ex.Message, MsgBoxStyle.Critical, "Gagal")
+            If SqlCon.State = ConnectionState.Open Then
+                SqlCon.Close()
+            End If
+        End Try
+    End Sub
+End Class
---
+++ Belajar/Belajar/SQLControl.vb
@@ -0,0 +1,19 @@
+﻿Imports MySql.Data.MySqlClient
+Module SQLControl
+
+    Dim SERVER As String = "127.0.0.1"
+    Dim PORT As String = "3306"
+    Dim UID As String = "root"
+    Dim PASSWORD As String = ""
+    Dim DATABASE As String = "db_belajar"
+
+    Dim ConnectionString As String = "SERVER=" & SERVER & ";PORT=" & PORT & ";UID=" & UID & ";PASSWORD=" & PASSWORD & ";DATABASE=" & DATABASE & ""
+
+    Public SqlCon As New MySqlConnection(ConnectionString)
+
+    Public SQLCMD As MySqlCommand
+    Public SQLDA As MySqlDataAdapter
+    Public SQLDS As DataSet
+
+
+End Module
